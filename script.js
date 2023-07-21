497660ed65345c649f9bfbf3a63efad03a00aff7
jof <joofran42@gmail.com>
2023-07-21 11:19:13 -0500

    add siguiente estado&intercambiarReproduccion

--- script.js
+++ script.js
@@ -2,7 +2,42 @@
 let filas = 80;
 let columnas = 80;
 let lado = 20;
+let reproducir = false;
 let fotografia = [];
+
+document.addEventListener("keydown", (e) => {
+    // For keyboard control
+    e.preventDefault();
+    switch (e.keyCode) {
+      case 39:
+        siguienteEstado();
+        break;
+      case 32:
+        intercambiarReproduccion();
+        break;
+      case 8:
+        limpiar();
+        break;
+      default:
+        break;
+    }
+  });
+  setInterval(()=>{
+    if(reproducir){
+        siguienteEstado()
+    }
+  },200);
+function intercambiarReproduccion() {
+    reproducir = !reproducir
+    if (reproducir) {
+        document.body.style.background = "white"
+        document.getElementById("btn1").innerHTML = `<i class="fas fa-pause"></i>`
+    } else {
+        document.body.style.background = "#f0f0ff"
+        document.getElementById("btn1").innerHTML = `<i class="fas fa-play"></i>`
+    }
+}
+
 generarTablero()
 function generarTablero(){
     let html = "<table cellpadding=0 cellspacing=0 id ='tablero'>"
@@ -30,7 +65,14 @@ function cambiarEstado(x, y) {
             celula.style.background = ""
     }
 }
-
+function limpiar() {
+    for (let x = 0; x < columnas; x++) {
+        for (let y = 0; y < filas; y++) {
+            let celula = document.getElementById(`celula-${x + "-" + y}`)
+            celula.style.background = ""
+        }
+}
+}
 function fotografiar() {
     fotografia = []
     for(let x=0; x < columnas; x++){
@@ -42,3 +84,40 @@ function fotografiar() {
         
     }
 }
+function contarVivas(x, y){
+    let vivas = 0
+    for (let i=-1; i<=1; i++){
+        for(let j=-1; j<=1;j++){
+            if(i==0 && j==0)
+                continue
+            try{
+                if(fotografia[x+i][y+j])
+                    vivas++
+                
+            }catch(e){ }
+            if(vivas>3){
+                return vivas
+            }
+        }
+    }
+    return vivas
+}
+function siguienteEstado(){
+    fotografiar()
+    for(let x=0; x < columnas; x++){
+        for (let y =0; y <columnas; y++){
+            let vivas = contarVivas(x,y)
+            let celula = document.getElementById(`celula-${x + "-" + y}`)
+            if(fotografia[x][y]){
+                if(vivas<2 || vivas >3)
+                    //muere por sobrepoblacion
+                    celula.style.background = ""
+            }else{
+                if(vivas ==3)
+                    celula.style.background ="black"
+            }
+        }
+        
+    }
+}
+
