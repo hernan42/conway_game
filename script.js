eb2a0bdbd5a32bd636e476b58ec189b54b8e4e49
jof <joofran42@gmail.com>
2023-07-21 10:05:22 -0500

    add cambiarEstado

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 let filas = 80;
 let columnas = 80;
 let lado = 20;
-
+let fotografia = [];
 generarTablero()
 function generarTablero(){
     let html = "<table cellpadding=0 cellspacing=0 id ='tablero'>"
-    for (let f = 0 ; f < filas; f++){
+    for (let y = 0 ; y < filas; y++){
         html += "<tr>"
-        for (let c=0; c < columnas; c++){
-            html += "<td>"
+        for (let x=0; x < columnas; x++){
+            html += `<td id= "celula-${x+"-" +y}" onmouseup="cambiarEstado(${x}, ${y})">`
             html += "</td>"
         }
         html += "</tr>"
@@ -21,4 +21,24 @@ function generarTablero(){
     let tablero = document.getElementById("tablero")
     tablero.style.width = lado*columnas + "px"
     tablero.style.height = lado*filas + "px"
+}
+function cambiarEstado(x, y) {
+    let celula = document.getElementById(`celula-${x + "-" + y}`)
+    if (celula.style.background != "black") {
+        celula.style.background = "black"
+    } else {
+            celula.style.background = ""
+    }
+}
+
+function fotografiar() {
+    fotografia = []
+    for(let x=0; x < columnas; x++){
+        fotografia.push([])
+        for (let y =0; y <columnas; y++){
+            let celula = document.getElementById(`celula-${x + "-" + y}`)
+            fotografia[x][y] =celula.style.background =="black"
+        }
+        
+    }
 }
