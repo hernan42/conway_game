5301250c2dadc37e21f4a1b3f708fffe54d6017d
Jofran Tito <39636048+jof42@users.noreply.github.com>
2023-07-22 14:25:08 -0500

    random&min&plus

--- script.js
+++ script.js
@@ -1,33 +1,72 @@
+let filas = 150
+let columnas = 150
+let lado = 15
 
-let filas = 80;
-let columnas = 80;
-let lado = 20;
-let reproducir = false;
-let fotografia = [];
+let reproducir = false
 
-document.addEventListener("keydown", (e) => {
-    // For keyboard control
-    e.preventDefault();
+let fotografia = []
+
+document.addEventListener("keydown", (e) => { //Para tener un control con el teclado
+    e.preventDefault() //Para evitar el movimiento del scroll
     switch (e.keyCode) {
-      case 39:
-        siguienteEstado();
-        break;
-      case 32:
-        intercambiarReproduccion();
-        break;
-      case 8:
-        limpiar();
-        break;
-      default:
-        break;
+        case 39:
+            siguienteEstado()
+            break;
+        case 32:
+            intercambiarReproducción()
+            break;
+        case 8:
+            limpiar()
+            break;
+        default:
+            break;
     }
-  });
-  setInterval(()=>{
-    if(reproducir){
+})
+
+setInterval(() => {
+    if (reproducir) {
         siguienteEstado()
     }
-  },200);
-function intercambiarReproduccion() {
+}, 1000 / 60);
+
+function centrar() {
+    window.scrollTo(
+        (lado * columnas - window.innerWidth) / 2,
+        (lado * filas - window.innerHeight) / 2
+    )
+}
+
+function minus() {
+    lado--
+    if (lado <= 7) {
+        lado = 7
+        return
+    }
+    let tablero = document.getElementById("tablero")
+    tablero.style.width = lado * columnas + "px"
+    tablero.style.height = lado * filas + "px"
+}
+
+function plus() {
+    lado++
+    let tablero = document.getElementById("tablero")
+    tablero.style.width = lado * columnas + "px"
+    tablero.style.height = lado * filas + "px"
+}
+
+function randomizar() {
+    mapa_complejidad = []
+    for (let x = 0; x < columnas; x++) {
+        for (let y = 0; y < filas; y++) {
+            if (Math.random() < 0.2) {
+                cambiarEstado(x, y)
+            }
+        }
+    }
+}
+
+function intercambiarReproducción() {
+    mapa_complejidad = []
     reproducir = !reproducir
     if (reproducir) {
         document.body.style.background = "white"
@@ -39,85 +78,148 @@ function intercambiarReproduccion() {
 }
 
 generarTablero()
-function generarTablero(){
-    let html = "<table cellpadding=0 cellspacing=0 id ='tablero'>"
-    for (let y = 0 ; y < filas; y++){
+
+function generarTablero() {
+    let html = "<table cellpadding=0 cellspacing=0 id='tablero'>"
+    for (let y = 0; y < filas; y++) {
         html += "<tr>"
-        for (let x=0; x < columnas; x++){
-            html += `<td id= "celula-${x+"-" +y}" onmouseup="cambiarEstado(${x}, ${y})">`
+        for (let x = 0; x < columnas; x++) {
+            html += `<td id="celula-${x + "-" + y}" onmouseup="cambiarEstado(${x}, ${y});mapa_complejidad = []">`
             html += "</td>"
         }
         html += "</tr>"
     }
-
     html += "</table>"
     let contenedor = document.getElementById("contenedor-tablero")
     contenedor.innerHTML = html
     let tablero = document.getElementById("tablero")
-    tablero.style.width = lado*columnas + "px"
-    tablero.style.height = lado*filas + "px"
+    tablero.style.width = lado * columnas + "px"
+    tablero.style.height = lado * filas + "px"
+    centrar()
 }
+
 function cambiarEstado(x, y) {
     let celula = document.getElementById(`celula-${x + "-" + y}`)
     if (celula.style.background != "black") {
         celula.style.background = "black"
     } else {
-            celula.style.background = ""
+        celula.style.background = ""
     }
 }
+
 function limpiar() {
+    mapa_complejidad = []
     for (let x = 0; x < columnas; x++) {
         for (let y = 0; y < filas; y++) {
             let celula = document.getElementById(`celula-${x + "-" + y}`)
             celula.style.background = ""
         }
-}
-}
-function fotografiar() {
-    fotografia = []
-    for(let x=0; x < columnas; x++){
-        fotografia.push([])
-        for (let y =0; y <columnas; y++){
-            let celula = document.getElementById(`celula-${x + "-" + y}`)
-            fotografia[x][y] =celula.style.background =="black"
-        }
-        
+    }
+    if (reproducir) {
+        intercambiarReproducción()
     }
 }
-function contarVivas(x, y){
+
+let mapa_complejidad = []
+let p_mapa_complejidad = []
+let mapa_verificados = []
+
+function fotografiar() {
+    p_mapa_complejidad = JSON.parse(JSON.stringify(mapa_complejidad));
+    mapa_complejidad = []
+    mapa_verificados = []
+    fotografia = []
+    if (!p_mapa_complejidad.length) {
+        primeraFoto()
+    } else {
+        demasFotos()
+    }
+}
+
+
+function demasFotos() {
+    for (let x in p_mapa_complejidad) {
+        for (let y in p_mapa_complejidad[x]) {
+            try {
+                let celula = document.getElementById(`celula-${x + "-" + y}`)
+                if (!fotografia[x]) {
+                    fotografia[x] = []
+                    mapa_verificados[x] = []
+                }
+                fotografia[x][y] = celula.style.background == "black"
+                calcularMapaComplejidad(Number(x), Number(y))
+            } catch (e) { }
+        }
+    }
+    p_mapa_complejidad = []
+}
+
+function primeraFoto() {
+    for (let x = 0; x < columnas; x++) {
+        fotografia.push([])
+        mapa_verificados.push([])
+        for (let y = 0; y < filas; y++) {
+            let celula = document.getElementById(`celula-${x + "-" + y}`)
+            fotografia[x][y] = celula.style.background == "black"
+            calcularMapaComplejidad(x, y)
+        }
+    }
+}
+
+function calcularMapaComplejidad(x, y) {
+    if (fotografia[x][y]) {
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if (!mapa_complejidad[x + i]) {
+                    mapa_complejidad[x + i] = []
+                }
+                mapa_complejidad[x + i][y + j] = true
+            }
+        }
+    }
+}
+
+function contarVivas(x, y) {
     let vivas = 0
-    for (let i=-1; i<=1; i++){
-        for(let j=-1; j<=1;j++){
-            if(i==0 && j==0)
+    for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+            if (i == 0 && j == 0) {
                 continue
-            try{
-                if(fotografia[x+i][y+j])
+            }
+            try {
+                if (fotografia[x + i][y + j]) {
                     vivas++
-                
-            }catch(e){ }
-            if(vivas>3){
+                }
+            } catch (e) { }
+            if (vivas > 3) {
                 return vivas
             }
         }
     }
     return vivas
 }
-function siguienteEstado(){
+
+function siguienteEstado() {
     fotografiar()
-    for(let x=0; x < columnas; x++){
-        for (let y =0; y <columnas; y++){
-            let vivas = contarVivas(x,y)
-            let celula = document.getElementById(`celula-${x + "-" + y}`)
-            if(fotografia[x][y]){
-                if(vivas<2 || vivas >3)
-                    //muere por sobrepoblacion
-                    celula.style.background = ""
-            }else{
-                if(vivas ==3)
-                    celula.style.background ="black"
-            }
+    for (const x in mapa_complejidad) {
+        for (const y in mapa_complejidad[x]) {
+            try {
+                if (mapa_verificados[x][y]) {
+                    continue
+                }
+                mapa_verificados[x][y] = true
+
+                let vivas = contarVivas(Number(x), Number(y))
+                let celula = document.getElementById(`celula-${x + "-" + y}`)
+                if (fotografia[x][y]) { //celula está viva
+                    if (vivas < 2 || vivas > 3) {
+                        celula.style.background = "" // Muere por sobrepoblación o soledad
+                    }
+                } else { //celula está muerta
+                    if (vivas == 3)
+                        celula.style.background = "black"
+                }
+            } catch (e) { }
         }
-        
     }
 }
-
